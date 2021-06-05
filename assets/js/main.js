e5f3759e3bd2cd2ee0609b09362edb4eeea45e5d
Muhammad Azri (c) <muhammad.azri@avanade.com>
2021-06-05 15:33:22 +0800

    Add qualification section

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,12 +47,29 @@ function toggleSkills() {
 }
 
 skillsHeader.forEach((el) => {
-    console.log(el);
     el.addEventListener('click', toggleSkills)
 })
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+       tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach( tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach( tabsContent => {
+            tabsContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+
+        tab.forEach( tab => {
+            tab.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualification__active');
+    })
+});
 
 
 /*==================== SERVICES MODAL ====================*/
