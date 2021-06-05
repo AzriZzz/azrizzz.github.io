986005432cad2bda74413b8ce2959ac9768e2ffd
Muhammad Azri (c) <muhammad.azri@avanade.com>
2021-06-05 22:38:58 +0800

    add section: project in mind

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,18 @@ modalCloses.forEach( (modalCLose) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
