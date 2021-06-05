0ab7c2cb841279c2ed473eb924015d1dd525a696
Muhammad Azri (c) <muhammad.azri@avanade.com>
2021-06-05 23:23:46 +0800

    add new section: testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,7 @@ modalCloses.forEach( (modalCLose) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
@@ -111,6 +111,22 @@ let swiper = new Swiper(".portfolio__container", {
   });
 
 /*==================== TESTIMONIAL ====================*/
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true
+    },
+    breakpoints: {
+        568: {
+            slidersPerView: 2
+        }
+    }
+  });
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
