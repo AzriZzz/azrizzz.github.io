85688ce586a26ea8fe6b80e9446b9e144f4b817e
Muhammad Azri (c) <lenovorosak@gmail.com>
2021-06-07 21:58:59 +0800

    fix the js error

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,8 +198,9 @@ if ( selectedTheme ) {
 // Activate / Deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
+
     document.body.classList.toggle(darkTheme);
-    themeButton.body.classList.toggle(iconTheme);
+    themeButton.classList.toggle(iconTheme);
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
